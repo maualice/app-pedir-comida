e7373f5588ce63af55abbdd2b4d588195b73dcd5
Mauro Alice <alicemauroe@gmail.com>
2025-02-18 20:35:11 -0300

    barra de navegacion inferior

---
+++ src/app/core/components/tabs/tabs.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  standalone: false,
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.scss'
+})
+export class TabsComponent {
+
+  constructor(private router: Router) {
+  }
+
+  colorDesactivado = "#555555"
+  colorActivado = "#000000"
+
+  navegar(direccion: string) {
+    this.router.navigate([direccion])
+  }
+}
